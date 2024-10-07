88903c574488fa03fb8b0d84a3da2d2fd8ef2c43
Allandantas21 <Allandantas21.ad@gmail.com>
2024-10-07 15:14:37 -0300

    add history

--- src/main.py
+++ src/main.py
@@ -2,9 +2,8 @@ from validator import valid_prompt
 from exec import simple_exec
 from env import getenv
 from exec import executor
-import platform
 import os
-import sys
+import readline
 
 def main():
     env = os.environ
@@ -13,10 +12,11 @@ def main():
         cmds = list()
         curr_path = os.getcwd().replace(os.environ['HOME'], '~')
         prompt = input(f"{curr_path}$> ")
+        readline.add_history(prompt)
         if not valid_prompt(prompt):
             continue
         executor(prompt, env)
-      
+    readline.clear_history()
 
 if __name__ == "__main__":
     main()
